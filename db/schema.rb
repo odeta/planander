6f54de1ad2af95e3cbada7dc17c10d5281e244a0
lechwolowski <lech.wolowski@gmail.com>
2019-01-10 23:15:56 +0100

    Style update

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190108220200) do
+ActiveRecord::Schema.define(version: 20190110220333) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
