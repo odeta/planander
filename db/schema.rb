dbb6a01298756a93bb0b13932c66c24476dfbc98
Odeta <odeta.kizyte@gmail.com>
2019-01-11 15:23:36 +0200

    Remove name attribute from users table

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20190111130359) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
