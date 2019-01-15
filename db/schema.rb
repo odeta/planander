e2144a5a66a88e3002ee67f8dce304626ad5ad9c
lechwolowski <lech.wolowski@gmail.com>
2019-01-15 16:58:11 +0100

    deleted and corrected naming

--- db/schema.rb
+++ db/schema.rb
@@ -33,18 +33,6 @@ ActiveRecord::Schema.define(version: 20190114181325) do
     t.index ["calendar_id"], name: "index_events_on_calendar_id"
   end
 
-  create_table "installs", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_installs_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
