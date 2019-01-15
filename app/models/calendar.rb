a61d0b3e593960490ef49462ac81c3c7e30d6717
Odeta Kizytė <odeta.kizyte@gmail.com>
2019-01-15 10:57:44 +0100

    Fix calendar validation

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,8 +1,8 @@
 class Calendar < ApplicationRecord
   belongs_to :user
   has_many :events, dependent: :destroy
-  validates :color, length: { is: 6 },
-                    format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :color, length: { is: 7 },
+                    format: { with: /\A#[a-fA-F0-9]+\z/ }
   validates :title, length: { in: 1..20 }
   validates :title, :color, :user_id, presence: true
 end
